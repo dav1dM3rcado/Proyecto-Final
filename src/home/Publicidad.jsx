7859f3da38bd63b0765cfd6090434ddce2cfd98d
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2022-02-21 14:39:34 -0300

    se agrego los componentes de publicidad y planes para mascotas

---
+++ src/home/Publicidad.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import publicidad from "./img/publicidad.video.mp4"
+import "./style/PublicidadVideo.css"
+
+const Publicidad = () => {
+    return (
+        <div className='my-5 container'>
+        <h2 className='text-center fw-bold publicidad-h2'>Alimentacion de alta calidad</h2>
+        <div className=''>
+            <video className='publicidadVideoHome' src={publicidad} alt="publicidad" autoPlay loop muted></video>
+        </div>
+        </div>
+    );
+};
+
+export default Publicidad;
